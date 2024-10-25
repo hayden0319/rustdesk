908d336655169b5ef1d9f3568bded5c12ba12735
hayden0319 <128303115+hayden0319@users.noreply.github.com>
2024-10-25 19:32:21 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -205,14 +205,214 @@ sudo chown "${uname}" -R /var/log/rustdesk/
 rustdesksignal="$(cat << EOF
 [Unit]
 Description=Rustdesk Signal Server
-After=network.target
-
 [Service]
 Type=simple
+LimitNOFILE=1000000
 ExecStart=/opt/rustdesk/hbbs
+WorkingDirectory=/opt/rustdesk/
+User=${uname}
+Group=${gname}
 Restart=always
-
+StandardOutput=append:/var/log/rustdesk/signalserver.log
+StandardError=append:/var/log/rustdesk/signalserver.error
+# Restart service after 10 seconds if node service crashes
+RestartSec=10
 [Install]
 WantedBy=multi-user.target
 EOF
 )"
+echo "${rustdesksignal}" | sudo tee /etc/systemd/system/rustdesksignal.service > /dev/null
+sudo systemctl daemon-reload
+sudo systemctl enable rustdesksignal.service
+sudo systemctl start rustdesksignal.service
+
+# Setup Systemd to launch hbbr
+rustdeskrelay="$(cat << EOF
+[Unit]
+Description=Rustdesk Relay Server
+[Service]
+Type=simple
+LimitNOFILE=1000000
+ExecStart=/opt/rustdesk/hbbr
+WorkingDirectory=/opt/rustdesk/
+User=${uname}
+Group=${gname}
+Restart=always
+StandardOutput=append:/var/log/rustdesk/relayserver.log
+StandardError=append:/var/log/rustdesk/relayserver.error
+# Restart service after 10 seconds if node service crashes
+RestartSec=10
+[Install]
+WantedBy=multi-user.target
+EOF
+)"
+echo "${rustdeskrelay}" | sudo tee /etc/systemd/system/rustdeskrelay.service > /dev/null
+sudo systemctl daemon-reload
+sudo systemctl enable rustdeskrelay.service
+sudo systemctl start rustdeskrelay.service
+
+while ! [[ $CHECK_RUSTDESK_READY ]]; do
+  CHECK_RUSTDESK_READY=$(sudo systemctl status rustdeskrelay.service | grep "Active: active (running)")
+  echo -ne "Rustdesk Relay not ready yet...${NC}\n"
+  sleep 3
+done
+
+pubname=$(find /opt/rustdesk -name "*.pub")
+key=$(cat "${pubname}")
+
+echo "Tidying up install"
+if [ "${ARCH}" = "x86_64" ] ; then
+rm rustdesk-server-linux-amd64.zip
+rm -rf amd64
+elif [ "${ARCH}" = "armv7l" ] ; then
+rm rustdesk-server-linux-armv7.zip
+rm -rf armv7
+elif [ "${ARCH}" = "aarch64" ] ; then
+rm rustdesk-server-linux-arm64v8.zip
+rm -rf arm64v8
+fi
+
+echo "Grabbing installers"
+string="{\"host\":\"${wanip}\",\"relay\":\"${wanip}\",\"key\":\"${key}\",\"api\":\"https://${wanip}\"}"
+string64=$(echo -n "$string" | base64 -w 0 | tr -d '=')
+string64rev=$(echo -n "$string64" | rev)
+
+echo "$string64rev"
+
+function setuphttp () {
+    # Create windows install script
+    wget https://raw.githubusercontent.com/dinger1986/rustdeskinstall/master/WindowsAgentAIOInstall.ps1
+    sudo sed -i "s|secure-string|${string64rev}|g" WindowsAgentAIOInstall.ps1
+
+    # Create linux install script
+    wget https://raw.githubusercontent.com/dinger1986/rustdeskinstall/master/linuxclientinstall.sh
+    sudo sed -i "s|secure-string|${string64rev}|g" linuxclientinstall.sh
+
+    # Download and install gohttpserver
+    # Make Folder /opt/gohttp/
+    if [ ! -d "/opt/gohttp" ]; then
+        echo "Creating /opt/gohttp"
+        sudo mkdir -p /opt/gohttp/
+        sudo mkdir -p /opt/gohttp/public
+    fi
+    sudo chown "${uname}" -R /opt/gohttp
+    cd /opt/gohttp
+    GOHTTPLATEST=$(curl https://api.github.com/repos/codeskyblue/gohttpserver/releases/latest -s | grep "tag_name"| awk '{print substr($2, 2, length($2)-3) }')
+
+    echo "Installing Go HTTP Server"
+    if [ "${ARCH}" = "x86_64" ] ; then
+    wget "https://github.com/codeskyblue/gohttpserver/releases/download/${GOHTTPLATEST}/gohttpserver_${GOHTTPLATEST}_linux_amd64.tar.gz"
+    tar -xf  gohttpserver_${GOHTTPLATEST}_linux_amd64.tar.gz 
+    elif [ "${ARCH}" =  "aarch64" ] ; then
+    wget "https://github.com/codeskyblue/gohttpserver/releases/download/${GOHTTPLATEST}/gohttpserver_${GOHTTPLATEST}_linux_arm64.tar.gz"
+    tar -xf  gohttpserver_${GOHTTPLATEST}_linux_arm64.tar.gz
+    elif [ "${ARCH}" = "armv7l" ] ; then
+    echo "Go HTTP Server not supported on 32bit ARM devices"
+    echo -e "Your IP/DNS Address is ${wanip}"
+    echo -e "Your public key is ${key}"
+    exit 1
+    fi
+
+    # Copy Rustdesk install scripts to folder
+    mv /opt/rustdesk/WindowsAgentAIOInstall.ps1 /opt/gohttp/public/
+    mv /opt/rustdesk/linuxclientinstall.sh /opt/gohttp/public/
+
+    # Make gohttp log folders
+    if [ ! -d "/var/log/gohttp" ]; then
+        echo "Creating /var/log/gohttp"
+        sudo mkdir -p /var/log/gohttp/
+    fi
+    sudo chown "${uname}" -R /var/log/gohttp/
+
+    echo "Tidying up Go HTTP Server Install"
+    if [ "${ARCH}" = "x86_64" ] ; then
+    rm gohttpserver_"${GOHTTPLATEST}"_linux_amd64.tar.gz
+    elif [ "${ARCH}" = "armv7l" ] || [ "${ARCH}" =  "aarch64" ]; then
+    rm gohttpserver_"${GOHTTPLATEST}"_linux_arm64.tar.gz
+    fi
+
+
+    # Setup Systemd to launch Go HTTP Server
+    gohttpserver="$(cat << EOF
+[Unit]
+Description=Go HTTP Server
+[Service]
+Type=simple
+LimitNOFILE=1000000
+ExecStart=/opt/gohttp/gohttpserver -r ./public --port 8000 --auth-type http --auth-http admin:${admintoken}
+WorkingDirectory=/opt/gohttp/
+User=${uname}
+Group=${gname}
+Restart=always
+StandardOutput=append:/var/log/gohttp/gohttpserver.log
+StandardError=append:/var/log/gohttp/gohttpserver.error
+# Restart service after 10 seconds if node service crashes
+RestartSec=10
+[Install]
+WantedBy=multi-user.target
+EOF
+)"
+    echo "${gohttpserver}" | sudo tee /etc/systemd/system/gohttpserver.service > /dev/null
+    sudo systemctl daemon-reload
+    sudo systemctl enable gohttpserver.service
+    sudo systemctl start gohttpserver.service
+
+
+    echo -e "Your IP/DNS Address is ${wanip}"
+    echo -e "Your public key is ${key}"
+    echo -e "Install Rustdesk on your machines and change your public key and IP/DNS name to the above"
+    echo -e "You can access your install scripts for clients by going to http://${wanip}:8010"
+    echo -e "Username is admin and password is ${admintoken}"
+    if [[ -z "$http" ]]; then
+        echo "Press any key to finish install"
+        while [ true ] ; do
+        read -t 3 -n 1
+        if [ $? = 0 ] ; then
+        exit ;
+        else
+        echo "waiting for the keypress"
+        fi
+        done
+        break
+    fi
+}
+
+# Choice for Extras installed
+if [[ -z "$http" ]]; then
+    PS3='Please choose if you want to download configs and install HTTP server:'
+    EXTRA=("Yes" "No")
+    select EXTRAOPT in "${EXTRA[@]}"; do
+    case $EXTRAOPT in
+    "Yes")
+    setuphttp
+    break
+    ;;
+    "No")
+    echo -e "Your IP/DNS Address is ${wanip}"
+    echo -e "Your public key is ${key}"
+    echo -e "Install Rustdesk on your machines and change your public key and IP/DNS name to the above"
+
+    echo - e "You can get a free API with Addressbook etc via https://github.com/infiniteremote/installer"
+
+    echo "Press any key to finish install"
+    while [ true ] ; do
+    read -t 3 -n 1
+    if [ $? = 0 ] ; then
+    exit ;
+    else
+    echo "waiting for the keypress"
+    fi
+    done
+    break
+    ;;
+    *) echo "invalid option $REPLY";;
+    esac
+    done
+elif [ "$http" = "true" ]; then
+    setuphttp
+elif [ "$http" = "false" ]; then
+    echo -e "Your IP/DNS Address is ${wanip}"
+    echo -e "Your public key is ${key}"
+    echo -e "Install Rustdesk on your machines and change your public key and IP/DNS name to the above"
+
+    echo - e "You can get a free API with Addressbook etc via https://github.com/infiniteremote/installer"
